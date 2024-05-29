441b352fd1f9ff5c486c26b4de4f439fd902a4e9
OpenWave-GW <openwave.gw@gmail.com>
2024-05-29 09:44:55 +0800

    Added trigger functions

--- lib/dso_trigger.py
+++ lib/dso_trigger.py
@@ -19,7 +19,7 @@ class Trigger():
         self.query = parent.query #for DSO SCPI
 
     def freq(self) -> float:
-        """Current frequency counter.
+        """Queries the trigger frequency.
 
         Returns:
             float: frequency
@@ -28,36 +28,36 @@ class Trigger():
         return float(ret)
 
     def type(self) -> str:
-        """Current trigger type.
+        """Queries the trigger type.
 
         Returns:
-            str: edge, delay, pulse, video, runt, risefall, bus, logic or timeout
+            str: EDGE|DELAY|PULSEWIDTH|VIDEO|RUNT|RISEFALL|LOGIC|BUS|TIMEOUT
         """
         cmd=':TRIG:TYP?\n'
         ret = self.query(cmd)
         return ret
 
     def set_type(self, type:str):
-        """Set trigger type.
+        """Sets the trigger type.
 
         Args:
-            type (str): edge, delay, pulse, video, runt, risefall, bus, logic or timeout
+            type (str): EDGe|DELay|PULSEWidth|VIDeo|RUNT|RISEFall|LOGic|BUS|TIMEOut
         """
         cmd=':TRIG:TYP %s\n'%type
         self.write(cmd)
 
     def source(self) -> str:
-        """Current trigger source.
+        """Queries the trigger source.
 
         Returns:
-            str: trigger source
+            str: CH1|CH2|CH3|CH4|EXT|LINE|D0|D1|D2|D3|D4|D5|D6|D7|D8|D9|D10|D11|D12|D13|D14|D15
         """
         cmd=':TRIG:SOUR?\n'
         ret = self.query(cmd)
         return ret
 
     def set_source(self, src:str):
-        """Set trigger source
+        """Sets the trigger source.
 
         Args:
             src (str): CH1|CH2|CH3|CH4|EXT|LINe|D0|D1|D2|D3|D4|D5|D6|D7|D8|D9|D10|D11|D12|D13|D14|D15
@@ -66,17 +66,17 @@ class Trigger():
         self.write(cmd)
 
     def mode(self) -> str:
-        """Current trigger mode.
+        """Queries the trigger mode.
 
         Returns:
-            str: AUTo|NORMal
+            str: AUTO|NORMAL
         """
         cmd=':TRIG:MOD?\n'
         ret = self.query(cmd)
         return ret
 
     def set_mode(self, mode:str):
-        """Set trigger mode.
+        """Sets the trigger mode.
 
         Args:
             mode (str): AUTo|NORMal
@@ -85,7 +85,7 @@ class Trigger():
         self.write(cmd)
 
     def coupling(self) -> str:
-        """Current trigger coupling
+        """Queries the trigger coupling.
 
         Returns:
             str: DC|AC|HF|LF
@@ -95,7 +95,7 @@ class Trigger():
         return ret
 
     def set_coupling(self, mode:str):
-        """Set trigger coupling.
+        """Sets the trigger coupling.
 
         Args:
             mode (str): DC|AC|HF|LF
@@ -103,8 +103,60 @@ class Trigger():
         cmd=':TRIG:COUP %s\n'%mode
         self.write(cmd)
 
+
+    def slope(self) -> str:
+        """Queries the edge/delay/rise&fall trigger slope.
+
+        Returns:
+            str: RISE|FALL|EITHER
+        """
+        trig_type = self.type()
+        if trig_type.lower() in ['edge', 'delay', 'risefall']:
+            cmd=':TRIG:%s:SLOP?\n'%trig_type
+            ret = self.query(cmd)
+            return ret
+        else:
+            return 'Unsupported trigger type for slope query: %s' % trig_type
+
+    def set_slope(self, type:str):
+        """Sets the edge/delay/rise&fall trigger slope.
+
+        Args:
+            type (str): RISe|FALL|EITher
+        """
+        trig_type = self.type()
+        if trig_type.lower() in ['edge', 'delay', 'risefall']:
+            cmd=':TRIG:%s:SLOP %s\n'%(trig_type,type)
+            self.write(cmd)
+
+    def polarity(self) -> str:
+        """Queries the pulse width/runt/video trigger polarity.
+
+        Returns:
+            str: POSITIVE|NEGATIVE
+        """
+        trig_type = self.type()
+        if trig_type.lower() in ['pulsewidth', 'runt', 'video']:
+            cmd=':TRIG:%s:POL?\n'%trig_type
+            ret = self.query(cmd)
+            return ret
+        else:
+            return 'Unsupported trigger type for polarity query: %s' % trig_type
+
+    def set_polarity(self, type:str):
+        """Sets the pulse width/runt/video trigger polarity.
+
+        Args:
+            type (str): POSitive|NEGative
+        """
+        trig_type = self.type()
+        if trig_type.lower() in ['pulsewidth', 'runt', 'video']:
+            cmd=':TRIG:%s:POL %s\n'%(trig_type,type)
+            self.write(cmd)
+
     def level(self) -> float:
-        """Current trigger level
+        """Queries the trigger level.        
+           Note: Not applicable to Pulse Runt and Rise & Fall triggers.
 
         Returns:
             float: trigger level
@@ -114,7 +166,8 @@ class Trigger():
         return float(ret)
 
     def set_level(self, value:float):
-        """Set trigger level
+        """Sets the trigger level.
+           Note: Not applicable to Pulse Runt and Rise & Fall triggers.
 
         Args:
             value (float): trigger level
@@ -122,8 +175,52 @@ class Trigger():
         cmd=':TRIG:LEV %f\n'%value
         self.write(cmd)
 
+
+    def hlevel(self) -> float:
+        """Queries the high trigger level.        
+           Note: Applicable for Rise and Fall/Pulse Runt triggers.
+
+        Returns:
+            float: trigger high level
+        """
+        cmd=':TRIG:HLEV?\n'
+        ret = self.query(cmd)
+        return float(ret)
+
+    def set_hlevel(self, value:float):
+        """Sets the high trigger level.
+           Note: Applicable for Rise and Fall/Pulse Runt triggers.
+
+        Args:
+            value (float): trigger high level
+        """
+        cmd=':TRIG:HLEV %f\n'%value
+        self.write(cmd)
+        
+    def llevel(self) -> float:
+        """Queries the low trigger level.        
+           Note: Applicable for Rise and Fall/Pulse Runt triggers.
+
+        Returns:
+            float: trigger low level
+        """
+        cmd=':TRIG:LLEV?\n'
+        ret = self.query(cmd)
+        return float(ret)
+
+    def set_llevel(self, value:float):
+        """Sets the low trigger level.
+           Note: Applicable for Rise and Fall/Pulse Runt triggers.
+
+        Args:
+            value (float): trigger low level
+        """
+        cmd=':TRIG:LLEV %f\n'%value
+        self.write(cmd)
+
+
     def external_probe_ratio(self) -> float:
-        """Current external probe ratio
+        """Queries the external probe ratio.
 
         Returns:
             float: external probe ratio
@@ -133,7 +230,7 @@ class Trigger():
         return float(ret)
 
     def set_external_probe_ratio(self, value:float):
-        """Set external probe ratio
+        """Sets the external probe ratio.
 
         Args:
             value (float): external probe ratio
@@ -150,19 +247,19 @@ class Trigger():
             return False
 
     def set_noise_rejection_on(self):
-        """Set trigger noise rejection ON.
+        """Sets noise rejection on.
         """
         cmd = ':TRIGger:NREJ ON'
         self.write(cmd)
 
     def set_noise_rejection_off(self):
-        """Set trigger noise rejection OFF.
+        """Sets noise rejection off.
         """
         cmd = ':TRIGger:NREJ OFF'
         self.write(cmd)
 
     def holdoff(self) -> float:
-        """Current holdoff time.
+        """Queries the holdoff time.
 
         Returns:
             float: holdoff time
@@ -172,7 +269,7 @@ class Trigger():
         return float(ret)
 
     def set_holdoff(self, value:float):
-        """Set holdoff time.
+        """Sets the holdoff time.
 
         Args:
             value (float): holdoff time
